9abbb6b6691c96feb2b3d5403babed7ae331ee4a
Humberto Ramone <humbertorody@gmail.com>
2021-08-17 11:40:19 -0300

    -Adicionando arquivo JavaScript

--- js/validaForm.js
+++ js/validaForm.js
@@ -1,3 +1,15 @@
 function validaForm(){
     
+    let name = document.querySelector("#name").value
+    let email = document.querySelector("#email").value
+    let assunto = document.querySelector("#inlineFormInput").value
+    let msg = document.querySelector("#exampleTextarea").value
+    
+    
+    if(name == null || name == ""){
+        alert("Verificar campo nome")
+    }
+    else if (name < 2){
+        alert("Campo nome muito curto")
+    }
 }
